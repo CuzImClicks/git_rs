1cee1efedd04acb8868622a453c84bd8853cbef9
Clicks <58398364+CuzImClicks@users.noreply.github.com>
2023-09-26 13:17:08 +0000

    added create function

--- src/repository.rs
+++ src/repository.rs
@@ -1,11 +1,11 @@
 use core::panic;
-use std::path::PathBuf;
+use std::{path::PathBuf, fs::File, io::Write};
 
 
 pub struct Repository {
     worktree: PathBuf,
     gitdir: PathBuf,
-    //conf: &'a Path
+    initialised: bool
 }
 
 impl Repository {
@@ -17,32 +17,69 @@ impl Repository {
         new_path
     }
 
-    fn repo_file(&self, path: Vec<PathBuf>) -> PathBuf {
-
+    fn repo_file(&self, path: Vec<PathBuf>, mkdir: bool) -> Result<PathBuf, String> {
+        if let Ok(_) = match mkdir {
+            true => self.repo_dir_create(path.split_last().unwrap().1.to_vec()),
+            false => self.repo_dir(path.split_last().unwrap().1.to_vec())
+        } {
+            return Ok(self.repo_path(path));
+        }
+        Err(format!("{:?}", path))
     }
 
-    fn repo_dir(&self, path: Vec<PathBuf>, mkdir: bool) -> bool {
+    fn repo_dir(&self, path: Vec<PathBuf>) -> Result<PathBuf, String> {
+        let p = self.repo_path(path);
+
+        if p.exists() || p.is_dir() {
+            return Ok(p);
+        }
+        return Err(format!("'{:?}' is not a directory", p).to_string());
+    }
+
+    fn repo_dir_create(&self, path: Vec<PathBuf>) -> Result<PathBuf, String> {
         let p = self.repo_path(path);
 
         if p.exists() {
             if p.is_dir() {
-                return true;
+                return Ok(p);
             }
-
+            return Err(format!("'{:?}' is not a directory", p).to_string());
         }
-        if mkdir {
-            std::fs::create_dir_all(p).unwrap();
-            return true;
-        }
-        return false
+        std::fs::create_dir_all(p.clone()).unwrap();
+        return Ok(p);
     }
 
-    fn new(path: PathBuf, force: bool) -> Repository {
-        let git_path: PathBuf = path.join(".git");
-        let repo = Repository { worktree: path.clone(), gitdir: git_path };
-        if !(force || repo.gitdir.is_dir()) {
-            panic!("Not a git repository!");
+    fn create(&self) -> Result<(), String> {
+        if self.worktree.exists() {
+            if self.worktree.read_dir().unwrap().count() != 0 {
+                return Err("Directory is not empty!".to_string());
+            }
+        } else {
+            std::fs::create_dir(self.worktree.clone()).expect("Failed to create directory");
         }
+        self.repo_dir_create(vec![PathBuf::from("branches")])?;
+        self.repo_dir_create(vec![PathBuf::from("objects")])?;
+        self.repo_dir_create(vec![PathBuf::from("refs"), PathBuf::from("tags")])?;
+        self.repo_dir_create(vec![PathBuf::from("refs"), PathBuf::from("heads")])?;
+        
+        let mut description = File::create(self.repo_file(vec![PathBuf::from("description")], false)?).expect("Failed to create description file.");
+        let _ = description.write_all("Unnamed repository; edit this file 'description' to name the repository.\n".as_bytes()).expect("Failed to write description file");
+        
+        let mut head = File::create(self.repo_file(vec![PathBuf::from("HEAD")], false)?).expect("Failed to create HEAD file.");
+        let _ = head.write_all("ref: refs/heads/master\n".as_bytes()).expect("Failed to write HEAD file.");
+
+        Ok(())
+
+
+    }
+
+    fn new(path: PathBuf) -> Repository {
+        let git_path: PathBuf = path.join(".git");
+        if !path.is_file() {
+            panic!("Repository is a file!");
+        }
+        let repo = Repository { worktree: path.clone(), gitdir: git_path.clone(), initialised: git_path.is_dir() };
+        
 
         repo
     }
