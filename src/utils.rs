38b5c1472f7e81ca1c1d4775e70e98fcd2252947
CuzImClicks <58398364+CuzImClicks@users.noreply.github.com>
2023-10-04 18:35:27 +0200

    added Set

--- src/utils.rs
+++ src/utils.rs
@@ -45,3 +45,69 @@ pub fn crlf_to_lf<T: AsRef<[u8]>>(data: &T) -> Vec<u8> {
     }
     result
 }
+
+pub struct Set<T> {
+    items: Vec<T>
+}
+
+impl <T>Set<T> {
+    pub fn remove(&mut self, index: usize) -> T {
+        self.items.remove(index)
+    }
+
+    pub fn add(&mut self, item: T)
+        where T: PartialEq {
+        if self.contains(&item) {
+            return;
+        }
+        self.items.push(item);
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn contains(&self, item: &T) -> bool
+        where T: PartialEq {
+        self.items.contains(item)
+    }
+
+    pub fn new() -> Self {
+        Self {
+            items: Vec::new()
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        self.items.get(index)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<T> {
+        self.items.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<T> {
+        self.items.iter_mut()
+    }
+
+    pub fn remove_item(&mut self, item: &T) -> Option<T>
+        where T: PartialEq {
+        let index = self.items.iter().position(|x| x == item);
+        index.map(|index| self.remove(index))
+    }
+
+    pub fn clear(&mut self) {
+        self.items.clear();
+    }
+
+    pub fn append<G: AsRef<Vec<T>>>(&mut self, other: &G)
+        where T: Clone + PartialEq {
+        for item in other.as_ref().iter() {
+            self.add(item.clone());
+        }
+    }
+}
